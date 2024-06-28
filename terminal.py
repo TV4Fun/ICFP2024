b2777ab63aa72ae54e28ce9a436aebb3293f962f
Joel Croteau <joelcroteau@joelcroteau.com>
2024-06-28 11:17:04 -0600

    Add terminal app

---
+++ terminal.py
@@ -0,0 +1,22 @@
+import requests
+from traceback import print_exc
+
+from icfp.encode import encode_string
+from icfp.decode import decode_message
+
+
+URL = "https://boundvariable.space/communicate"
+AUTH_HEADER = {
+    "Authorization": "Bearer f8fb3b34-7f8f-4cb0-bd74-c83be464d0a1"
+}
+
+while True:
+    try:
+        command = input("> ")
+        payload = encode_string(command)
+        response = requests.post(URL, data=payload, headers=AUTH_HEADER)
+        print(decode_message(response.text))
+    except (KeyboardInterrupt, EOFError):
+        exit(0)
+    except:
+        print_exc()
