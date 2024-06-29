e5e8b38975b218780b1d095fbe958534906128f8
deilann <yandoryn@gmail.com>
2024-06-28 20:04:08 -0600

    something's broken and i'm eepy, graph isn't right

--- lambdaman/lambdagraph.py
+++ lambdaman/lambdagraph.py
@@ -1,37 +1,29 @@
 import numpy as np
 import networkx as nx
+from itertools import product
 
 lambda_map = """
-#...#.#.........#...
-#.###.#.#####.###.##
-#...#.#.....#.......
-###.#.#.###.########
-#.#....L..#.#.......
-#.#####.###.#.###.##
-#.#.#...#.......#...
-#.#.#######.#######.
-#.#...#.#...#.#.....
-#.#.###.#.###.###.#.
-#.....#...#.......#.
-#.###.###.###.#####.
-#.#.#...#...#...#...
-###.#.#.#.#####.###.
-#...#.#...#.....#...
-#.###.#.#.#####.####
-#.....#.#.....#.#...
-#.###.#.#.#.#.#.#.##
-#.#...#.#.#.#.#.....
+.....########...
+....#...........
+...#..######....
+..#..#......#...
+.#..#...##...#..
+.#..#..#L.#...#.
+.#...#....#...#.
+..#...####...#..
+...#........#...
+....########....
+................
 """
 
-lambda_map = lambda_map.split("\n")
-lambda_map = [[c for c in line] for line in lambda_map if line]
-lambda_map = np.array(lambda_map)
-print(lambda_map)
+lambda_list = lambda_map.split("\n")
+lambda_list = [[c for c in line] for line in lambda_list if line]
+lambda_map = np.array(lambda_list)
 
 lambda_man = np.where(lambda_map == "L")
 lambda_man = (int(lambda_man[0][0]), int(lambda_man[1][0]))
 
-
+nodes = [(int(x), int(y)) for x, y in zip(np.where(lambda_map == ".")[0], np.where(lambda_map == ".")[1])]
 def find_neighbors(node):
     neighbors = []
     possibilities = [[node[0] + 1, node[1]], [node[0], node[1] + 1], [node[0] - 1, node[1]], [node[0], node[1] - 1]]
@@ -40,29 +32,35 @@ def find_neighbors(node):
             continue
         if option[0] > lambda_map.shape[0] - 1 or option[1] > lambda_map.shape[1] - 1:
             continue
-
         neighbor = lambda_map[option[0]][option[1]]
         if neighbor == ".":
             neighbors.append(tuple(option))
     return neighbors
 
-nodes = [(int(x), int(y)) for x, y in zip(np.where(lambda_map == ".")[0], np.where(lambda_map == ".")[1])]
-root_neighbors = find_neighbors(lambda_man)
-G = {lambda_man: root_neighbors}
+G = {lambda_man:find_neighbors(lambda_man)}
+
 for node in nodes:
     neighbors = find_neighbors(node)
-    G[node] = neighbors
+    if neighbors:
+        G[node] = neighbors
 
 G = nx.from_dict_of_lists(G)
+
+H = nx.minimum_spanning_tree(G)
+print(len(G.nodes))
+print(lambda_man)
+
+I = nx.complete_graph(H)
 path = nx.approximation.traveling_salesman_problem(G, cycle=False)
 
+print(path)
 def stringify(path):
     nodes = path
 
     origin = nodes.pop()
     string = ""
     while nodes:
-        next = nodes.pop()
+        next = nodes.pop(0)
         if origin[0] - next[0] == 1:
             string += "U"
         elif origin[0] - next[0] == -1:
@@ -75,4 +73,4 @@ def stringify(path):
 
     return string
 
-print(stringify(path))
+
