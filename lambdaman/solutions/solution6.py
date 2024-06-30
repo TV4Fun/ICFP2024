9d97b9d500164598c14ae8f8c7761c1a8ea5100a
deilann <yandoryn@gmail.com>
2024-06-30 04:32:27 -0600

    it was so not what was used the fstring was even wrong

--- lambdaman/solutions/solution6.py
+++ lambdaman/solutions/solution6.py
@@ -12,6 +12,6 @@ lazy_tsp = G.lazy_tsp()
 
 
 # this is not the method used to solve
-print(f"solve lambdaman5 {lazy_tsp}")
+print(f"solve lambdaman6 {lazy_tsp}")
 
 # B$ L! B$ v! B$ v! B$ v! SLLLL L! B. B. v! v! B. v! v!
