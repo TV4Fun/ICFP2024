bf35c2290955dd4ce1926e63807bab81d0d0b36d
deilann <yandoryn@gmail.com>
2024-06-30 03:05:04 -0600

    modified to class and added lazy path

--- lambdaman/lambdagraph.py
+++ lambdaman/lambdagraph.py
@@ -8,42 +8,129 @@ import numpy as np
 'R': {1, 3, 5, 7, 9, 11, 13, 15}
 """
 
+
+
+
 WAYS = {
-    "U": {0b1000, 0b1001, 0b1010, 0b1011, 0b1100, 0b1101, 0b1110, 0b1111},
-    "D": {0b0100, 0b0101, 0b0110, 0b0111, 0b1100, 0b1101, 0b1110, 0b1111},
-    "L": {0b0010, 0b0011, 0b0110, 0b0111, 0b1010, 0b1011, 0b1110, 0b1111},
-    "R": {0b0001, 0b0011, 0b0101, 0b0111, 0b1001, 0b1011, 0b1101, 0b1111},
+    1: "R",
+    2: "L",
+    3: "LR",
+    4: "D",
+    5: "DR",
+    6: "LD",
+    7: "LDR",
+    8: "U",
+    9: "UR",
+    10: "LU",
+    11: "LUR",
+    12: "UD",
+    13: "UDR",
+    14: "LUD",
+    15: "LUDR"
 }
+
+DIRECTIONS = {
+    "U": (-1, 0),
+    "D": (1, 0),
+    "L": (0, -1),
+    "R": (0, 1),
+}
+
+
+class LambdaMap:
+    def __init__(self, map_file):
+        self.map, self.origin = create_array(map_file)
+        self.pills = [(int(x), int(y)) for x, y in zip(*np.nonzero(self.map)) if (int(x), int(y))]
+        self.max_distance = 1
+        self.lazy_path = self.walk()
+
+    def walk(self):
+        distances = dict(zip(self.pills, ["K" for _ in self.pills]))
+        ways = WAYS[self.map[self.origin]]
+        distances[self.origin] = ""
+        visited = [self.origin]
+        for way in ways:
+            coord = (DIRECTIONS[way][0] + self.origin[0], DIRECTIONS[way][1] + self.origin[1])
+            distances[coord] = way
+            visited.append(coord)
+
+        while any(distance == "K" for distance in distances.values()):
+            queue = [coord for coord in visited if len(distances[coord]) == self.max_distance and distances[coord] != "K"]
+            while queue:
+                coord = queue.pop()
+                path = distances[coord]
+                ways = WAYS[self.map[coord]]
+                for way in ways:
+                    coords = (DIRECTIONS[way][0] + coord[0], DIRECTIONS[way][1] + coord[1])
+                    if distances[coords] == "K":
+                        new_path = path + way
+                        distances[coords] = new_path
+                        self.max_distance = len(new_path)
+                        visited.append(coords)
+        paths = sorted(list(distances.values()), key=lambda x: len(x), reverse=True)
+        combined_paths = []
+        print(paths)
+
+
+        while paths:
+            path = paths.pop()
+            if not any(other_path.startswith(path) for other_path in paths):
+                combined_paths.append(path)
+
+        full_path = ""
+        if len(combined_paths) > 1:
+            for path in combined_paths:
+                reverse_path = path[::-1].translate(str.maketrans("UDLR", "DURL"))
+                full_path += path + reverse_path
+        else:
+            full_path = combined_paths[0]
+
+        return full_path
+
+
+
+
+
+
 
 
 def create_array(map_file: str) -> tuple[np.array, tuple[int, int]]:
     with open(map_file) as f:
         lines = f.read().splitlines()
     map_shape = (len(lines), len(lines[0]))
-    map_array = np.full(map_shape, 0b1111)
+    map_array = np.full(map_shape, 0b1111, dtype=int)
     lambda_man = (0, 0)
 
     for row, line in enumerate(lines):
         for col, char in enumerate(line):
+            if row == 0:
+                map_array[row, col] &= 0b0111
+            if row == len(lines) - 1:
+                map_array[row, col] &= 0b1011
+            if col == 0:
+                map_array[row, col] &= 0b1101
+            if col == len(lines[0]) - 1:
+                map_array[row, col] &= 0b1110
+
             if char == "L":
                 lambda_man = (row, col)
             elif char == "#":
-                print(char)
-                print(row, col)
                 # U
-                if row > 0:
-                    map_array[row - 1, col] &= 0b0111
-                # D
                 if row < map_shape[0] - 1:
+                    map_array[row + 1, col] &= 0b0111
+                # D
+                if row > 0:
                     map_array[row - 1, col] &= 0b1011
                 # L
-                if col > 0:
-                    map_array[row, col - 1] &= 0b1101
-                # R
                 if col < map_shape[1] - 1:
-                    map_array[row, col + 1] &= 0b1110
+                    map_array[row, col + 1] &= 0b1101
+                # R
+                if col > 0:
+                    map_array[row, col - 1] &= 0b1110
 
                 map_array[row, col] = 0b0000
+
+
 
     return map_array, lambda_man
 
