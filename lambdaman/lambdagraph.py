40431c94885f6e90f3aad57cee7d4539aa6d4081
deilann <yandoryn@gmail.com>
2024-06-29 23:41:09 -0600

    update create_graph to create_array to create bitstring edge array

--- lambdaman/lambdagraph.py
+++ lambdaman/lambdagraph.py
@@ -1,44 +1,51 @@
-import networkx as nx
+# import networkx as nx
 import numpy as np
 
+"""
+'U': {8, 9, 10, 11, 12, 13, 14, 15}
+'D': {4, 5, 6, 7, 12, 13, 14, 15}
+'L': {2, 3, 6, 7, 10, 11, 14, 15}
+'R': {1, 3, 5, 7, 9, 11, 13, 15}
+"""
 
-def create_graph(map_file: str) -> nx.Graph:
-    def find_neighbors(node):
-        neighbors = []
-        x, y = node
-        possibilities = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
-        for row, col in possibilities:
-            if 0 <= row < lambda_map.shape[0] and 0 <= col < lambda_map.shape[1]:
-                neighbor = lambda_map[row, col]
-                if not neighbor:
-                    neighbors.append((row, col))
-        return neighbors
+WAYS = {
+    "U": {0b1000, 0b1001, 0b1010, 0b1011, 0b1100, 0b1101, 0b1110, 0b1111},
+    "D": {0b0100, 0b0101, 0b0110, 0b0111, 0b1100, 0b1101, 0b1110, 0b1111},
+    "L": {0b0010, 0b0011, 0b0110, 0b0111, 0b1010, 0b1011, 0b1110, 0b1111},
+    "R": {0b0001, 0b0011, 0b0101, 0b0111, 0b1001, 0b1011, 0b1101, 0b1111},
+}
 
+
+def create_array(map_file: str) -> tuple[np.array, tuple[int, int]]:
     with open(map_file) as f:
         lines = f.read().splitlines()
+    map_shape = (len(lines), len(lines[0]))
+    map_array = np.full(map_shape, 0b1111)
+    lambda_man = (0, 0)
 
     for row, line in enumerate(lines):
         for col, char in enumerate(line):
             if char == "L":
                 lambda_man = (row, col)
-                break
-        else:
-            continue
-        break
-    lambda_map = np.array([[c == "#" for c in line] for line in lines], dtype=np.bool)
+            elif char == "#":
+                print(char)
+                print(row, col)
+                # U
+                if row > 0:
+                    map_array[row - 1, col] &= 0b0111
+                # D
+                if row < map_shape[0] - 1:
+                    map_array[row - 1, col] &= 0b1011
+                # L
+                if col > 0:
+                    map_array[row, col - 1] &= 0b1101
+                # R
+                if col < map_shape[1] - 1:
+                    map_array[row, col + 1] &= 0b1110
 
-    nodes = [(int(x), int(y)) for x, y in zip(*np.nonzero(np.logical_not(lambda_map)))]
+                map_array[row, col] = 0b0000
 
-    G = {lambda_man: find_neighbors(lambda_man)}
-
-    for node in nodes:
-        neighbors = find_neighbors(node)
-        if neighbors:
-            G[node] = neighbors
-
-    G = nx.from_dict_of_lists(G)
-
-    return G
+    return map_array, lambda_man
 
 
 def stringify(path):
