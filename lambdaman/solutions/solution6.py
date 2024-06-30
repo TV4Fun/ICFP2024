8775a7d137611f493da5f9f13af80bcdc8736965
deilann <yandoryn@gmail.com>
2024-06-30 04:31:49 -0600

    edited to include the method definitely not used and the more efficient language

--- lambdaman/solutions/solution6.py
+++ lambdaman/solutions/solution6.py
@@ -8,4 +8,10 @@ L...............................................................................
 
 G = LambdaMap(map_file)
 
-lazy_path = G.lazy_path()
+lazy_tsp = G.lazy_tsp()
+
+
+# this is not the method used to solve
+print(f"solve lambdaman5 {lazy_tsp}")
+
+# B$ L! B$ v! B$ v! B$ v! SLLLL L! B. B. v! v! B. v! v!
