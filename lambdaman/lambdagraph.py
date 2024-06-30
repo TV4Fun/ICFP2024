492b152cf3efd376cdacff22384e29ef33fff6dc
deilann <yandoryn@gmail.com>
2024-06-29 21:43:48 -0600

    functioned create_graph for use in specific solutions

--- lambdaman/lambdagraph.py
+++ lambdaman/lambdagraph.py
@@ -1,55 +1,44 @@
-from icfp.encode import encode_digit, encode_char
-from icfp.decode import decode_int
 import networkx as nx
 import numpy as np
-from math import log
 
 
-map_file = "lambdaman5.txt"
+def create_graph(map_file: str) -> nx.Graph:
+    def find_neighbors(node):
+        neighbors = []
+        x, y = node
+        possibilities = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+        for row, col in possibilities:
+            if 0 <= row < lambda_map.shape[0] and 0 <= col < lambda_map.shape[1]:
+                neighbor = lambda_map[row, col]
+                if not neighbor:
+                    neighbors.append((row, col))
+        return neighbors
 
-with open(map_file) as f:
-    lines = f.read().splitlines()
+    with open(map_file) as f:
+        lines = f.read().splitlines()
 
-for row, line in enumerate(lines):
-    for col, char in enumerate(line):
-        if char == "L":
-            lambda_man = (row, col)
-            break
-    else:
-        continue
-    break
-lambda_map = np.array([[c == "#" for c in line] for line in lines], dtype=np.bool)
+    for row, line in enumerate(lines):
+        for col, char in enumerate(line):
+            if char == "L":
+                lambda_man = (row, col)
+                break
+        else:
+            continue
+        break
+    lambda_map = np.array([[c == "#" for c in line] for line in lines], dtype=np.bool)
 
-nodes = [(int(x), int(y)) for x, y in zip(*np.nonzero(np.logical_not(lambda_map)))]
+    nodes = [(int(x), int(y)) for x, y in zip(*np.nonzero(np.logical_not(lambda_map)))]
 
+    G = {lambda_man: find_neighbors(lambda_man)}
 
-def find_neighbors(node):
-    neighbors = []
-    x, y = node
-    possibilities = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
-    for row, col in possibilities:
-        if 0 <= row < lambda_map.shape[0] and 0 <= col < lambda_map.shape[1]:
-            neighbor = lambda_map[row, col]
-            if not neighbor:
-                neighbors.append((row, col))
-    return neighbors
+    for node in nodes:
+        neighbors = find_neighbors(node)
+        if neighbors:
+            G[node] = neighbors
 
+    G = nx.from_dict_of_lists(G)
 
-G = {lambda_man: find_neighbors(lambda_man)}
-
-for node in nodes:
-    neighbors = find_neighbors(node)
-    if neighbors:
-        G[node] = neighbors
-
-G = nx.from_dict_of_lists(G)
-
-H = nx.minimum_spanning_tree(G)
-
-
-I = nx.complete_graph(H)
-path = nx.approximation.traveling_salesman_problem(G, cycle=False)
-
+    return G
 
 
 def stringify(path):
@@ -70,5 +59,3 @@ def stringify(path):
         origin = next
 
     return string
-
-
